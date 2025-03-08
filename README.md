158de9bce3df9fe8e260f75fe06fb96c040686b9
queirozz8 <zeccakut@gmail.com>
2025-03-08 02:19:52 -0300

    ✨ feat: complete 'Create' button animation, menu, and UI improvements
    - 🎨 Added hover and click animations for the 'Create' button.
    - 📂 Completed the dropdown menu displayed when clicking the 'Create' button, including animations, colors, and styling.
    - 🔧 Fixed the size of the 'Create' button to make it larger.
    - 🔠 Added the correct font (Nunito).
    - 🗑 Removed an additional div above the input div that was not doing anything.
    - 🎛  Made the input value more controllable, managed by a React state.
    - 🖱  Added animations for button clicks, hover, etc., including colors and effects for elements like the 'Home', 'Install App', and 'View Premium Plans' buttons.
    - 🗑  Added a 'Clear All' button with proper animation, visible only when the input has content. Logic implemented.
    - 📍 Fixed the positioning of the 'Install App' button.
    - 🏷  Improved HTML semantics.
    - 📱 Enhanced input width responsiveness. Instead of using pixels, now it's using rem.
    - 🎨 Created variables for repeated hex color codes in TypeScript and CSS.
    - 🛠  Improved conditionals for showing certain classes on elements, replacing ternary operators with && for cases where the ternary previously returned an empty string.
    - 💡 Added comments for better code understanding.

--- README.md
+++ README.md
@@ -1,3 +1,106 @@
-<h1>Spotify Clone</h1>
+<h1>🎶 Spotify Rick - A Spotify Clone</h1>
 
-Made with TypeScript, React and Tailwind CSS (I used Lucide-React too).
+🔗 Site: https://spotify-rick.vercel.app
+<h2>🇺🇸 English: A Spotify clone made entirely by me.</h2>
+<h3>I just looked at the original Spotify's functionalities and re-created them in this Website.</h3>
+<h3>⚛ Was made using ReactJS with TypeScript on Vite, Tailwind CSS with Lucide-React and HTML</h3>
+<h3>🚀 Features:</h3>
+
+###
+
+- Count bills and coins based at their quantities;
+- Button to clear a single input;
+- Button to clear all inputs;
+- Counter showing how many bills and coins you have;
+- Control using the arrows next to the input fields;
+- Support to both computers and mobile devices.
+
+<h4>The goal is to help people who handle large amounts of cash and struggle to keep track of the total.</h4>
+
+### 
+
+<h2>🇧🇷 PT-BR: Um clone do Spotify feito interamente por mim.</h2>
+<h3>Eu só olhei as funcionalidades que o Spotify original possui e recriei elas nesse Website.</h3>
+<h3>⚛ Foi feito usando ReactJS com TypeScript no Vite, Tailwind CSS com Lucide-React e HTML</h3>
+<h3>🚀 Funcionalidades:</h3>
+
+###
+
+- Contar notas e moedas baseado nas suas quantidades;
+- Botão de apagar um input apenas;
+- Botão de apagar todos os inputs;
+- Contador de quantas notas e quantas moedas você possui;
+- Controle pelas setas ao lado do input;
+- Suporte a computadores e celulares.
+
+<h4>O objetivo é ajudar pessoas que possuem muitas quantidades de dinheiro vivo, que acabam se perdendo para contar todo aquele dinheiro.</h4>
+
+
+
+<h1>🇺🇸 DOCUMENTATION FOR LOCAL INSTALLATION</h1>
+
+1. First, do a git clone of this project:
+```
+git clone https://github.com/queirozz8/spotify-rick.git
+```
+2. Go to the directory of the project:
+```
+cd spotify-rick/
+```
+3.1. If you don't have pnpm installed, install it:
+```
+npm install -g pnpm@latest-10
+```
+3.2. Run this command to install all the dependencies (React, TypeScript, Vite, Tailwind CSS, Lucide-React, etc.):
+```
+pnpm install
+```
+4. Run the `build` script:
+```
+pnpm run build
+```
+5. Run the `preview` script:
+```
+pnpm run preview
+```
+6. Go to your browser and paste this URL:
+```
+http://localhost:3000
+```
+
+<h2>That's it! This project was made by Rick with ☕ and 💻.</h2>
+
+
+
+<h1>🇧🇷 DOCUMENTAÇÃO PARA INSTALAÇÃO LOCAL</h1>
+
+1. Primeiro, faça um git clone desse projeto:
+```
+git clone https://github.com/queirozz8/spotify-rick.git
+```
+2. Vá para o diretório do projeto:
+```
+cd spotify-rick/
+```
+3.1. Se você não tem o pnpm instalado, instale-o:
+```
+npm install -g pnpm@latest-10
+```
+3.2. Rode esse comando para instalar todas as dependências (React, TypeScript, Vite, Tailwind CSS, Lucide-React, etc.):
+```
+pnpm install
+```
+4. Rode o script `build`:
+```
+pnpm run build
+```
+5. Rode o script `preview`:
+```
+pnpm run preview
+```
+6. Vá para o seu navegador e cole esta URL:
+```
+http://localhost:3000
+```
+
+<h2>É isso! Este projeto foi criado por Rick com ☕ e 💻.</h2>
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,90 +1,235 @@
-import { useState } from 'react'
-import { House, Compass, Search, CircleArrowDown, Plus, Bell, User } from 'lucide-react'
+import { useState, ChangeEvent } from 'react'
+import { House, Compass, Search, CircleArrowDown,
+Plus, Bell, User, Music, Blend, Folder, X } from 'lucide-react'
 
 export default function Header() {
-  const [buttonText, setButtonText] = useState<string>('')
-  const [colorHouseButton, setColorHouseButton] = useState<string>('#989999')
-  const [colorInstallIcon, setColorInstallIcon] = useState<string>('#a1a1a1')
+  /* Criação de variáveis contendo o hex das cores mais usadas pelos elementos. Vale somente para o TypeScript */
+  const normalColor: string = '#989999'
+  const lightNormalColor: string = '#fffefe'
+  const clickedColor: string = '#7a7a7a'
 
-  /* Estou adicionando as classes por aqui, e não por `hover:` para que o botão consiga esperar pelo texto vindo. 
-  Se não, o botão fica com um tamanho e posição alterados (que se estabiliza quando o texto vem pelo TS) antes do TS adicionar o texto; 
-  o que faz com que o mouse saia de cima do botão, pois o botão mudou de lugar, porque o TS ainda não teve tempo de adicionar o texto;
-  com o mouse fora do botão, ele se recolhe e o texto não é exibido. Isso fica num loop eterno.
-  Então eu estou adicionando as classes pelo TS, para que tudo venha ao mesmo tempo e esse erro não ocorra. */
+  /* Estados dos elementos */
+  const [colorHouseButton, setColorHouseButton] = useState<string>(normalColor)
+  const [colorSearchButton, setColorSearchButton] = useState<string>('#b3b3b3')
+  const [inputValue, setInputValue] = useState<string>('')
+  const [colorCompassButton, setColorCompassButton] = useState<string>(normalColor)
+  const [colorCreateButton, setColorCreateButton] = useState<string>('#b3b3b3')
+  const [isCreateButtonClicked, setIsCreateButtonClicked] = useState<boolean>(false)
+  const [colorMusicIcon, setColorMusicIcon] = useState<string>(lightNormalColor)
+  const [colorBlendIcon, setColorBlendIcon] = useState<string>(lightNormalColor)
+  const [colorFolderIcon, setColorFolderIcon] = useState<string>(lightNormalColor)
+  const [colorInstallButton, setColorInstallButton] = useState<string>('#b3b3b3')
+  const [colorBellButton, setColorBellButton] = useState<string>(normalColor)
 
-  function handleMouseOverPlus(e: React.MouseEvent<HTMLButtonElement>) {
-    if (e.target instanceof HTMLElement) {
-      e.target.classList.remove('right-0')
-      e.target.classList.add('pr-6', '-right-15')
-      setButtonText('Criar')
-    }
-  }
-
-  function handleMouseLeavePlus(e: React.MouseEvent<HTMLButtonElement>) {
-    if (e.target instanceof HTMLElement) {
-      e.target.classList.remove('pr-6', '-right-15')
-      e.target.classList.add('right-0')
-      setButtonText('')
-    }
-  }
-
+  function handleInputChange(e: ChangeEvent<HTMLInputElement>) { setInputValue(e.target.value) }
 
   return (
-    <header className='flex justify-between items-center relative w-screen p-2'>
-      <img className='w-8 h-8 ml-5 cursor-pointer' title='Spotify' src="/spotify-white.png" alt="Spotify logo" />
+    /* Criação das variáveis de cor para o CSS também */
+    <header className='flex justify-between items-center sticky w-screen p-2 [--normal-color:#989999] [--clicked-color:#7a7a7a] [--light-normal-color:#fffefe]'>
 
-      <div className='flex justify-center items-center gap-3 absolute w-screen'>
-        <div className='flex justify-center items-center gap-1 relative left-5'>
+      {/* Ícone branco do Spotify no canto superior esquerdo */}
+      <a className='size-8 ml-5 cursor-pointer z-10' href="#">
+        <img title='Spotify' src="/spotify-white.png" alt="Spotify logo" />
+      </a>
+
+      {/* Div do input central com seus outros botões */}
+      <div className='flex justify-center items-center gap-1 absolute w-screen left-[5.1rem]'>
+        
+        {/* Botão de Início */}
+        <button
+          onMouseOver={ () => setColorHouseButton(prev => prev === clickedColor ? prev : lightNormalColor) } 
+          onMouseLeave={ () => setColorHouseButton(normalColor) }
+          onMouseDown={ () => setColorHouseButton(clickedColor) }
+          onMouseUp={ () => setColorHouseButton(normalColor) }
+          className='p-3 rounded-4xl bg-[#1f1f1f] cursor-pointer transition hover:bg-[#2b2b2a] hover:scale-105 active:scale-95 active:bg-[#1c1c1c]' 
+          title='Início'
+          type='button'> 
+            <House color={colorHouseButton} /> 
+        </button>
+
+        {/* Label/botão de busca */}
+        <label 
+          onMouseOver={ () => setColorSearchButton(lightNormalColor) } 
+          onMouseLeave={ () => setColorSearchButton(normalColor) } 
+          className='relative left-4 w-0 cursor-pointer' 
+          title='Buscar' 
+          htmlFor="search"> 
+            <Search color={colorSearchButton} />
+        </label>
+
+        {/* Input central */}
+        <input 
+          onChange={e => handleInputChange(e)}
+          value={inputValue}
+          className='w-[27.1rem] h-12 p-3 pt-3 pl-12 pr-24 text-zinc-100 bg-[#1f1f1f] border border-[#1f1f1f] rounded-3xl
+          placeholder:text-[#b3b3b3] transition-all duration-500 [&:hover,&:focus]:bg-[#2b2b2a] [&:hover,&:focus]:border-[#3d3c3d]'
+          placeholder="O que você quer ouvir?"
+          type="text" 
+          id="search" 
+        />
+
+        {/* Div dos outros botões adicionais do input */}
+        <div className='flex justify-center items-center gap-3 relative right-[6.3rem]' title='Navegar'>
+
+          {/* Botão de apagar tudo */}
           <button 
-            onMouseOver={ () => setColorHouseButton('#fffefe') } 
-            onMouseLeave={ () => setColorHouseButton('#989999') } 
-            className='p-3 rounded-4xl bg-[#1f1f1f] cursor-pointer transition hover:bg-[#2b2b2a] hover:scale-105' 
-            title='Início'
-            type='button'> 
-              <House color={colorHouseButton} /> 
+            onClick={ () => setInputValue('') } 
+            className={`transition ${inputValue !== '' ? 'opacity-100' : 'opacity-0'} cursor-pointer`} 
+            type='button'>
+              <X color={normalColor} size={30} strokeWidth={1.5} />
           </button>
 
-          <label className='relative left-4 w-0 cursor-pointer' title='Buscar' htmlFor="search"> <Search color='#989999' /> </label>
-          <input 
-            className='w-[434px] h-12 p-3 pl-12 pr-20 text-zinc-100 bg-[#1f1f1f] border border-[#1f1f1f] rounded-3xl
-            placeholder:text-[#c3c3c3] transition-all duration-500 [&:hover,&:focus]:bg-[#2b2b2a] [&:hover,&:focus]:border-[#3d3c3d]'
-            placeholder="O que você quer ouvir?"
-            type="text" 
-            id="search" 
-          />
+          {/* Linha vertical que separa o botão de apagar tudo do botão de Navegar */}
+          <svg xmlns="http://www.w3.org/2000/svg" width="1" height="26">
+            <line x1="0.5" y1="0" x2="0.5" y2="28" stroke="#767777" strokeWidth="1"/>
+          </svg>
 
-          <button
-            onMouseOver={e => handleMouseOverPlus(e)} 
-            onMouseLeave={e => handleMouseLeavePlus(e)} 
-            className='flex justify-center items-center gap-2 relative left-1 p-2 rounded-4xl text-[#cdcdcd] font-bold bg-[#1f1f1f] cursor-pointer transition duration-300 ease-in-out' 
-            title='Crie uma playlist, pasta ou Jam'
-            type='button'>
-              <Plus color='#989999' size={30} /> <span>{buttonText}</span>
+          {/* Botão de Navegar */}
+          <button 
+            onMouseOver={ () => setColorCompassButton(prev => prev === clickedColor ? prev : lightNormalColor) } 
+            onMouseLeave={ () => setColorCompassButton(normalColor) } 
+            onMouseDown={ () => setColorCompassButton(clickedColor) }
+            onMouseUp={ () => setColorCompassButton(normalColor) }
+            className='hover:scale-105 active:scale-95 transition cursor-pointer'
+            title='Navegar' 
+            type='button'> 
+              <Compass color={colorCompassButton} size={27} /> 
           </button>
         </div>
-        
-        <div className='flex justify-center items-center gap-3 relative right-24' title='Navegar'>
-          <div className='w-[1px] h-7 border border-[#767777]'></div>
-          <button className='cursor-pointer' title='Navegar' type='button'> <Compass color='#989999' size={27} /> </button>
+
+        {/* Wrapper para não permitir que o botão de Criar afete a o posicionamento dos elementos ao lado quando ele for hovered */}
+        <div className='relative right-[5.1rem] w-28'>
+
+          {/* Botão de Criar */}
+          <button
+            onMouseOver={ () => setColorCreateButton(lightNormalColor) }
+            onMouseLeave={ () => setColorCreateButton(normalColor) }
+            onClick={ () => setIsCreateButtonClicked(prev => !prev) }
+            onBlur={ () => setIsCreateButtonClicked(false) }
+            className={`flex justify-center items-center gap-2 p-2 pr-0 rounded-4xl text-[#cdcdcd] font-bold bg-[#1f1f1f] 
+              cursor-pointer group transition-all hover:pr-14 hover:bg-[#2b2b2a] ${isCreateButtonClicked && 'pr-14 bg-[#2b2b2a]'}`}
+            title='Crie uma playlist, pasta ou Jam'
+            type='button'>
+              <Plus 
+                className={`transition ${isCreateButtonClicked && 'rotate-45'}`} 
+                color={isCreateButtonClicked ? lightNormalColor : colorCreateButton} 
+                size={33}
+                strokeWidth={1.5}
+              />
+              <span className={`w-0 text-[var(--light-normal-color)] group-hover:text-base group-hover:opacity-100 ${isCreateButtonClicked ? 'opacity-100 text-base' : 'opacity-0 text-[0px]'} transition`}>
+                Criar
+              </span>
+          </button>
+
+          {/* Menu do botão de Criar - Só é exibido quando ele é clicado */}
+          <ul className={`flex flex-col items-start gap-4 absolute top-14 w-[23rem] p-2 bg-[#282828] rounded-xl ${isCreateButtonClicked ? 'opacity-100' : 'opacity-0'} transition`}>
+
+            {/* Opção de criar playlist */}
+            <li>
+              <button
+                onMouseOver={ () => setColorMusicIcon('#3dc46e') }
+                onMouseLeave={ () => setColorMusicIcon(lightNormalColor) }
+                className='flex items-center gap-2 w-[22rem] p-2 bg-[#282828] hover:bg-[#3f3e3e] active:bg-[#191818] rounded-md group'
+                type='button'>
+
+                {/* Círculo com o ícone */}
+                <div className='flex justify-center items-center relative size-12 bg-[#3e3f3e] group-hover:bg-[#525252] rounded-4xl'>
+                  <Music className='group-hover:rotate-6 group-hover:scale-105 transition' color={colorMusicIcon} />
+                </div>
+
+                <div> {/* Título e descrição */}
+                  <h2 className='flex items-center gap-2 text-[var(--light-normal-color)] font-bold'>Playlist</h2>
+                  <p className='text-[var(--normal-color)] text-sm'>Crie uma playlist com músicas ou episódios</p>
+                </div>
+              </button>
+            </li>
+
+            {/* Linha horizontal que separa o primeiro botão dos outros */}
+            <svg className='-m-5' xmlns="http://www.w3.org/2000/svg" viewBox="0 0 400 20">
+              <line x1="43" y1="10" x2="370" y2="10" stroke="#434444" strokeWidth="1"/>
+            </svg>
+
+            {/* Botão de criar Match */}
+            <li>
+              <button
+                onMouseOver={ () => setColorBlendIcon('#3dc46e') }
+                onMouseLeave={ () => setColorBlendIcon(lightNormalColor) }
+                className='flex items-center gap-2 w-[22rem] p-2 bg-[#282828] hover:bg-[#3f3e3e] active:bg-[#191818] rounded-md group'
+                type='button'>
+
+                {/* Círculo com o ícone */}
+                <div className='flex justify-center items-center relative size-12 bg-[#3e3f3e] group-hover:bg-[#525252] rounded-4xl'>
+                  <Blend className='group-hover:rotate-6 group-hover:-scale-x-105 -scale-x-100 transition' color={colorBlendIcon} />
+                </div>
+
+                <div> {/* Título e descrição */}
+                  <h2 className='flex items-center gap-2 text-[var(--light-normal-color)] font-bold'> Match</h2>
+                  <p className='text-[var(--normal-color)] text-sm'>Mistre seu gosto musical com o da galera</p>
+                </div>
+              </button>
+            </li>
+
+            {/* Botão de criar pasta */}
+            <li>
+              <button
+                onMouseOver={ () => setColorFolderIcon('#3dc46e') }
+                onMouseLeave={ () => setColorFolderIcon(lightNormalColor) }
+                className='flex items-center gap-2 w-[22rem] p-2 bg-[#282828] hover:bg-[#3f3e3e] active:bg-[#191818] rounded-md group'
+                type='button'>
+
+                {/* Círculo com o ícone */}
+                <div className='flex justify-center items-center relative size-12 bg-[#3e3f3e] group-hover:bg-[#525252] rounded-4xl'>
+                  <Folder className='group-hover:rotate-6 group-hover:scale-105 transition' color={colorFolderIcon} />
+                </div>
+
+                <div> {/* Título e descrição */}
+                  <h2 className='flex items-center gap-2 text-[var(--light-normal-color)] font-bold'>Pasta</h2>
+                  <p className='text-[var(--normal-color)] text-sm'>Organize suas playlists</p>
+                </div>
+              </button>
+            </li>
+          </ul>
         </div>
       </div>
 
+      {/* Div dos outros botões do Header. Engloba:
+      - Botões de "Ver planos Premium" e "Instalar aplicativo";
+      - Botão de Novidades
+      - Botão de Usuário */}
       <div className='flex justify-center items-center gap-6'>
+
+        {/* Div dos botões de "Ver planos Premium" e "Instalar aplicativo" */}
         <div className='flex justify-center items-center gap-6 relative right-2'>
-          <button className='flex justify-center items-center h-8 p-4 text-sm font-bold rounded-3xl bg-white cursor-pointer' type='button'>Ver planos Premium</button>
+
+          {/* Botão de "Ver planos Premium" */}
+          <button className='flex justify-center items-center w-[10.2rem] h-8 p-4 text-sm font-extrabold [word-spacing:-2px] rounded-3xl bg-white cursor-pointer hover:scale-105 active:scale-95 active:bg-[#c6c6c7] transition' type='button'>Ver planos Premium</button>
+
+          {/* Botão/Link de "Instalar aplicativo" */}
           <a 
-            href='#'
-            onMouseOver={ () => setColorInstallIcon('#fffefe') }
-            onMouseLeave={ () => setColorInstallIcon('#a1a1a1') }
-            className='flex justify-center items-center gap-2 text-[#a1a1a1] cursor-pointer text-sm font-bold hover:text-[#fffefe] transition-transform hover:scale-110'
-            draggable="false"> 
-              <span> <CircleArrowDown color={colorInstallIcon} size={18} /> </span>
+            onMouseOver={ () => setColorInstallButton(lightNormalColor) }
+            onMouseLeave={ () => setColorInstallButton(normalColor) }
+            onMouseDown={ () => setColorInstallButton(clickedColor) }
+            onMouseUp={ () => setColorInstallButton(lightNormalColor) }
+            className='flex justify-center items-center gap-[0.5rem] relative right-1 text-[#a1a1a1] cursor-pointer text-sm font-extrabold [word-spacing:-2px] hover:text-[var(--light-normal-color)] transition-transform hover:scale-105 active:scale-95 active:text-[var(--clicked-color)]'
+            href='#'>
+              <span> <CircleArrowDown color={colorInstallButton} size={17} /> </span>
               <span>Instalar aplicativo</span>
           </a>
         </div>
 
-        <button className='flex justify-center items-center cursor-pointer' title='Novidades'> <Bell color='#a1a1a1' size={18} /> </button>
-        <button className='p-3 bg-[#1f1f1f] rounded-4xl cursor-pointer' title='Usuário'>
+        {/* Botão de Novidades */}
+        <button
+          onMouseOver={ () => setColorBellButton(prev => prev === clickedColor ? prev : lightNormalColor) } 
+          onMouseLeave={ () => setColorBellButton(normalColor) } 
+          onMouseDown={ () => setColorBellButton(clickedColor) }
+          onMouseUp={() => setColorBellButton(normalColor) }
+          className='flex justify-center items-center cursor-pointer z-10 hover:scale-105 transition' 
+          title='Novidades'>
+             <Bell color={colorBellButton} size={18} />
+        </button>
+
+        {/* Botão de Usuário */}
+        <button className='p-3 bg-[#1f1f1f] rounded-4xl cursor-pointer z-10 hover:scale-105 active:scale-95 active:bg-[#181818] transition' title='Usuário'>
           <User color='#a1a1a1' size={25} />
         </button>
       </div>
