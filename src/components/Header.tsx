33e17215b905a3c0a611e38d6aaee4ca66ff3e2b
queirozz8 <zeccakut@gmail.com>
2025-03-24 00:40:20 -0300

    Modifying Header.tsx, Sidebar.tsx and creating CreateButton.tsx
    At the Header.tsx:
    - Reduced the number of lines, as the create button became a React component;
    - There is no more CSS variables being created at it;
    - It was removed the imported icons from lucide-react.
    
    CreateButton.tsx is a component for the Create Button (a button used both in the Header and in the Sidebar).
    It has differences between Header and Sidebar versions; witch is determined by the isSidebar prop.
    
    At the Sidebar.tsx:
    - Added initial visual elements (including the Create Button in your Sidebar version) with your initial respective CSS and TS;
    - Created initial React states for hex colors;
    - Created initial code comments.

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,6 +1,6 @@
 import { useState, ChangeEvent } from 'react'
-import { House, Compass, Search, CircleArrowDown, 
-Plus, Bell, User, Music, Blend, Folder, X,
+import CreateButton from './CreateButton'
+import { House, Compass, Search, CircleArrowDown, Bell, User, X,
 SquareArrowOutUpRight } from 'lucide-react'
 
 export default function Header() {
@@ -15,11 +15,6 @@ export default function Header() {
   const [colorSearchButton, setColorSearchButton] = useState<string>('#b3b3b3')
   const [inputValue, setInputValue] = useState<string>('')
   const [colorCompassButton, setColorCompassButton] = useState<string>(normalColor)
-  const [colorCreateButton, setColorCreateButton] = useState<string>('#b3b3b3')
-  const [isCreateButtonClicked, setIsCreateButtonClicked] = useState<boolean>(false)
-  const [colorMusicIcon, setColorMusicIcon] = useState<string>(lightNormalColor)
-  const [colorBlendIcon, setColorBlendIcon] = useState<string>(lightNormalColor)
-  const [colorFolderIcon, setColorFolderIcon] = useState<string>(lightNormalColor)
   const [colorInstallButton, setColorInstallButton] = useState<string>('#b3b3b3')
   const [colorBellButton, setColorBellButton] = useState<string>(normalColor)
   const [isUserButtonClicked, setIsUserButtonClicked] = useState<boolean>(false)
@@ -27,9 +22,7 @@ export default function Header() {
   function handleInputChange(e: ChangeEvent<HTMLInputElement>) { setInputValue(e.target.value) }
 
   return (
-    /* Criação das variáveis de cor para o CSS também 
-    Aqui, também é criada uma variável para as cores de background: bg-color */
-    <header className='flex justify-between items-center sticky w-screen p-2 [--normal-color:#989999] [--clicked-color:#7a7a7a] [--light-normal-color:#fffefe] [--bg-color:#282828]'>
+    <header className='flex justify-between items-center sticky w-screen p-2'>
 
       {/* Ícone branco do Spotify no canto superior esquerdo */}
       <a className='size-8 ml-5 cursor-pointer z-10' href="#">
@@ -105,112 +98,8 @@ export default function Header() {
           </button>
         </div>
 
-        {/* Wrapper para não permitir que o botão de Criar afete a o posicionamento dos elementos ao lado quando ele for hovered. */}
-        <div 
-          onBlur={ () => setIsCreateButtonClicked(false) }
-          className='relative right-[5.1rem] w-28'
-        >
-
-          {/* Botão de Criar */}
-          <button
-            onPointerOver={ () => setColorCreateButton(lightNormalColor) }
-            onPointerLeave={ () => setColorCreateButton(normalColor) }
-            onClick={ () => setIsCreateButtonClicked(prev => !prev) }
-            className={`flex justify-center items-center gap-2 p-2 pr-0 rounded-4xl text-[#cdcdcd] font-bold bg-[#1f1f1f] 
-              hover:pr-14 hover:bg-[#2b2b2a] ${isCreateButtonClicked && 'pr-14 bg-[#2b2b2a]'} transition-all cursor-pointer group`}
-            title='Crie uma playlist, pasta ou Jam'
-            type='button'
-          >
-            <Plus 
-              className={`${isCreateButtonClicked && 'rotate-45'} transition`} 
-              color={isCreateButtonClicked ? lightNormalColor : colorCreateButton} 
-              size={33}
-              strokeWidth={1.5}
-            />
-            <span className={`w-0 group-hover:text-base text-[var(--light-normal-color)] group-hover:opacity-100 ${isCreateButtonClicked ? 'opacity-100 text-base' : 'opacity-0 text-[0px]'} transition`}>
-              Criar
-            </span>
-          </button>
-
-          {/* Menu do botão de Criar - Só é exibido quando ele é clicado */}
-          <menu
-            onPointerDown={ (e) => e.preventDefault() } /* Faz com que a div maior não perca o foco e o menu não desapareça enquanto o usuário não tiver terminado de clicar */
-            /* Os displays não conflitam pois o block/hidden é adicionado dinâmicamente; ou seja, sobrescreve todos os outros displays existentes. */
-            className={`flex flex-col items-start gap-4 absolute top-14 ${isCreateButtonClicked ? 'block' : 'hidden'} w-[23rem] p-2 rounded-xl bg-[var(--bg-color)]`}
-          >
-
-            {/* Botão de criar playlist */}
-            <li><button
-              onPointerOver={ () => setColorMusicIcon('#3dc46e') }
-              onPointerDown={ (e) => e.preventDefault() } /* Faz com que a div maior não perca o foco e o menu não desapareça enquanto o usuário não tiver terminado de clicar */
-              onClick={ () => setIsCreateButtonClicked(false) } /* Evento para computadores */
-              onTouchEnd={ () => setIsCreateButtonClicked(false) } /* Evento para dispositivos móveis */
-              onPointerLeave={ () => setColorMusicIcon(lightNormalColor) }
-              className='flex items-center gap-2 w-[22rem] p-2 rounded-md bg-[var(--bg-color)] hover:bg-[#3f3e3e] active:bg-[#191818] group'
-              type='button'
-            >
-
-              {/* Círculo com o ícone */}
-              <div className='flex justify-center items-center size-12 rounded-4xl bg-[#3e3f3e] group-hover:bg-[#525252]'>
-                <Music className='group-hover:rotate-6 group-hover:scale-105 transition' color={colorMusicIcon} />
-              </div>
-
-              <span> {/* Título e descrição */}
-                <h1 className='flex items-center gap-2 text-[var(--light-normal-color)] font-bold'>Playlist</h1>
-                <p className='text-sm text-[var(--normal-color)]'>Crie uma playlist com músicas ou episódios</p>
-              </span>
-            </button></li>
-
-            {/* Linha horizontal que separa o primeiro botão dos outros */}
-            <svg className='-m-5' xmlns="http://www.w3.org/2000/svg" viewBox="0 0 400 20">
-              <line x1="43" y1="10" x2="370" y2="10" stroke="#434444" strokeWidth="1"/>
-            </svg>
-
-            {/* Botão de criar Match */}
-            <li><button
-              onPointerOver={ () => setColorBlendIcon('#3dc46e') }
-              onPointerDown={ (e) => e.preventDefault() } /* Faz com que a div maior não perca o foco e o menu não desapareça enquanto o usuário não tiver terminado de clicar */
-              onClick={ () => setIsCreateButtonClicked(false) } /* Evento para computadores */
-              onTouchEnd={ () => setIsCreateButtonClicked(false) } /* Evento para dispositivos móveis */
-              onPointerLeave={ () => setColorBlendIcon(lightNormalColor) }
-              className='flex items-center gap-2 w-[22rem] p-2 rounded-md bg-[var(--bg-color)] hover:bg-[#3f3e3e] active:bg-[#191818] group'
-              type='button'
-            >
-
-              {/* Círculo com o ícone */}
-              <div className='flex justify-center items-center size-12 rounded-4xl bg-[#3e3f3e] group-hover:bg-[#525252]'>
-                <Blend className='-scale-x-100 group-hover:rotate-6 group-hover:-scale-x-105 transition' color={colorBlendIcon} />
-              </div>
-
-              <span> {/* Título e descrição */}
-                <h1 className='flex items-center gap-2 text-[var(--light-normal-color)] font-bold'>Match</h1>
-                <p className='text-sm text-[var(--normal-color)]'>Mistre seu gosto musical com o da galera</p>
-              </span>
-            </button></li>
-
-            {/* Botão de criar pasta */}
-            <li><button
-              onPointerOver={ () => setColorFolderIcon('#3dc46e') }
-              onPointerDown={ (e) => e.preventDefault() } /* Faz com que a div maior não perca o foco e o menu não desapareça enquanto o usuário não tiver terminado de clicar */
-              onClick={ () => setIsCreateButtonClicked(false) } /* Evento para computadores */
-              onTouchEnd={ () => setIsCreateButtonClicked(false) } /* Evento para dispositivos móveis */
-              onPointerLeave={ () => setColorFolderIcon(lightNormalColor) }
-              className='flex items-center gap-2 w-[22rem] p-2 rounded-md bg-[var(--bg-color)] hover:bg-[#3f3e3e] active:bg-[#191818] group'
-              type='button'
-            >
-
-              {/* Círculo com o ícone */}
-              <div className='flex justify-center items-center size-12 rounded-4xl bg-[#3e3f3e] group-hover:bg-[#525252]'>
-                <Folder className='group-hover:rotate-6 group-hover:scale-105 transition' color={colorFolderIcon} />
-              </div>
-
-              <span> {/* Título e descrição */}
-                <h1 className='flex items-center gap-2 text-[var(--light-normal-color)] font-bold'>Pasta</h1>
-                <p className='text-sm text-[var(--normal-color)]'>Organize suas playlists</p>
-              </span>
-            </button></li>
-          </menu>
-        </div>
+        {/* Botão de Criar */}
+        <CreateButton isSidebar={false} />
       </search>
 
       {/* Seção dos outros botões do Header. Engloba:
--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -1,6 +1,42 @@
+import { useState } from "react"
+import CreateButton from "./CreateButton"
+import { LibraryBig, ArrowRight } from "lucide-react"
+
 export default function Sidebar() {
+  /* Criação de variáveis contendo o hex das cores mais usadas pelos elementos. 
+  Vale somente para o TypeScript */
+  const normalColor: string = '#989999'
+  const lightNormalColor: string = '#fffefe'
+
+  const [libraryColor, setLibraryColor] = useState<string>('#b3b3b3')
+  const [showMoreButtonColor, setShowMoreButtonColor] = useState<string>(normalColor)
+
   return (
-    <aside>
+    <aside className="flex flex-col gap-3 relative left-2 p-5 w-[17.5rem] rounded-lg bg-[#121212]">
+      <div className="flex justify-between items-center">
+        <button 
+          onPointerOver={ () => setLibraryColor(lightNormalColor) } 
+          onPointerLeave={ () => setLibraryColor(normalColor) }
+          className="flex gap-1 cursor-pointer group" 
+          title="Ocultar sua biblioteca"
+        >
+          <LibraryBig color={libraryColor} />
+          <h1 className="text-zinc-300 group-hover:text-[var(--light-normal-color)] font-extrabold">Sua Biblioteca</h1>
+        </button>
+
+        <div className="flex gap-3">
+          {/* Botão de Criar adaptado para a barra lateral */}
+          <CreateButton isSidebar={true} />
+
+          <button 
+            onPointerOver={ () => setShowMoreButtonColor(lightNormalColor) }
+            onPointerLeave={ () => setShowMoreButtonColor(normalColor) }
+            className="cursor-pointer"
+            title="Mostrar mais">
+            <ArrowRight color={showMoreButtonColor} />
+          </button>
+        </div>
+      </div>
     </aside>
   )
 }
